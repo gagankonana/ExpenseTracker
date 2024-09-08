55c592857226938b221a9f5cdf9618e45937d547
Gagan Nagaraj <gagandeep.konana@gmail.com>
2024-09-08 14:34:58 -0700

    Requirements

---
+++ expense.h
@@ -0,0 +1,30 @@
+#ifndef LIBRARY_EXPENSE_TRACKER_EXPENSE_H
+#define LIBRARY_EXPENSE_TRACKER_EXPENSE_H
+
+/*
+Design and implement an expense tracking system in C++. The system should allow users to manage their expenses with the following functionalities:
+
+requiremnts:
+    expense tracking system
+    add expense
+    delete expense 
+    list expenses
+    update expense
+    get total expense
+
+questions:
+    how will the user choose an expense? based on the id or the title?
+    do we need to store deleted expense in case user needs to undo?
+    What will expense info contain? what can users edit on it? : 
+
+classes:
+    Expense
+        id, desc, categpory, amount, date
+    User
+        username, other info
+    System
+        unordered_map<username, expense>
+
+*/
+
+#endif
